92f7c9c9301228641b4b2a67ebfc64973fb4d75c
Dao Van Sinh <Sinh2004tt@gmail.com>
2026-02-02 12:02:28 +0700

    cap nhap BookingServiceImpl.java

--- src/main/java/com/tourvn/service/impl/BookingServiceImpl.java
+++ src/main/java/com/tourvn/service/impl/BookingServiceImpl.java
@@ -4,24 +4,33 @@ import com.tourvn.dto.BookingRequest;
 import com.tourvn.entity.Booking;
 import com.tourvn.entity.BookingStatus;
 import com.tourvn.repository.BookingRepository;
+import com.tourvn.repository.TourRepository;
 import com.tourvn.service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.tourvn.entity.Tour;
 import java.time.LocalDateTime;
 
-@Service
+@Service    //tầng xử lý logic
 public class BookingServiceImpl implements BookingService {
 
-    @Autowired
+    @Autowired     //lưu database
     private BookingRepository bookingRepository;
 
+    @Autowired
+    private TourRepository tourRepository;
+
     @Override
-    public Booking createBooking(BookingRequest request) {
+    public Booking createBooking(BookingRequest request) {    // hàm xử lý tạo booking
 
-        Booking booking = new Booking();
+        Tour tour = tourRepository.findById(request.getTourId()).orElse(null);
+        if (tour == null) {
+            throw new IllegalArgumentException("Tour not found");
+        }
 
-        booking.setCustomerName(request.getCustomerName());
+        Booking booking = new Booking();  // tạo đối tượng booking
+
+        booking.setCustomerName(request.getCustomerName());    //gán tên client gọi dữ liệu từ request
         booking.setPhone(request.getPhone());
         booking.setEmail(request.getEmail());
         booking.setTourId(request.getTourId());
