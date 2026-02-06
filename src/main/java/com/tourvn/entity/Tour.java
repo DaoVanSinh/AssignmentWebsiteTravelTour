66874f19aadafffade21d621a53b0d4efe335cb6
trần mạnh toàn <tranmanhtoan@MacBook-Pro-cua-tran.local>
2026-02-06 14:10:50 +0700

    Sửa lỗi

--- src/main/java/com/tourvn/entity/Tour.java
+++ src/main/java/com/tourvn/entity/Tour.java
@@ -2,6 +2,7 @@ package com.tourvn.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tours")
@@ -13,13 +14,23 @@ public class Tour {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String title;
+    @Column(nullable = false, length = 150)
+    private String name; 
 
-    @Column(columnDefinition = "TEXT", nullable = true)
+    @Column(columnDefinition = "TEXT")
     private String description;
 
+    @Column(nullable = false)
     private Double price;
 
-    private String imageUrl;
+    @Column(name = "start_date")
+    private LocalDate startDate; 
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    @Column(name = "max_people")
+    private Integer maxPeople;
+
+    private String imageUrl; 
 }
